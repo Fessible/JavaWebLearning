5519bcad782a7b58b54b36520915f7f38d5f1716
fessible <499953902@qq.com>
2019-04-26 08:29:27 +0800

    hibernate

---
+++ springweb/src/bean/User.java
@@ -0,0 +1,15 @@
+package bean;
+
+
+
+public class User {
+
+    public void add(){
+        System.out.println("User add...");
+    }
+
+    public void update(){
+        System.out.println("user update...");
+    }
+
+}
