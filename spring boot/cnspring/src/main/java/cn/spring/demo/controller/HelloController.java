1a401f755b5f32fbb033172f07ec616b8f3fa6fc
fessible <499953902@qq.com>
2019-05-20 08:40:55 +0800

    springboot

---
+++ spring boot/cnspring/src/main/java/cn/spring/demo/controller/HelloController.java
@@ -0,0 +1,12 @@
+package cn.spring.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    String Hello(){
+        return "Hello world";
+    }
+}
