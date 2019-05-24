41a4ae300a51598d29081acbb3b6f7c8e9a4e373
rhm <499953902@qq.com>
2019-05-24 17:44:46 +0800

    添加日志文件

---
+++ spring boot/demo/src/main/java/com/example/demo/entity/Dog.java
@@ -0,0 +1,36 @@
+package com.example.demo.entity;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "dog")
+public class Dog {
+    private String name;
+
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ spring boot/demo/src/main/java/com/example/demo/service/HelloService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+public class HelloService {
+
+    public HelloService() {
+        System.out.println("创建HelloService对象");
+    }
+}
