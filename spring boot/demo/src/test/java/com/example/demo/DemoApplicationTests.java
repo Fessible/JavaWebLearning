2eb687d32624a8f77de5ef74306deeec6f79ba8f
fessible <499953902@qq.com>
2019-05-25 08:49:26 +0800

    更新

--- spring boot/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ spring boot/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DemoApplicationTests {
 
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    /**
+     * 日志默认级别由地到高 trace<debug<info<warn<error
+     */
     @Test
     public void contextLoads() {
+        logger.trace("---trace");
+        logger.debug("---debug");
+        logger.info("---info");
+        logger.warn("---warn");
+        logger.error("---error");
     }
-
 }
