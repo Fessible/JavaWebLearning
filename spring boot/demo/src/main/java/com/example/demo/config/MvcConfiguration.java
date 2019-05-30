e1e6f326b79c900bb254d92f8da474b736afa276
fessible <499953902@qq.com>
2019-05-30 08:22:11 +0800

    添加server配置

--- spring boot/demo/src/main/java/com/example/demo/config/MvcConfiguration.java
+++ spring boot/demo/src/main/java/com/example/demo/config/MvcConfiguration.java
@@ -1,11 +1,26 @@
 package com.example.demo.config;
 
+import org.springframework.boot.autoconfigure.web.embedded.EmbeddedWebServerFactoryCustomizerAutoConfiguration;
+import org.springframework.boot.web.server.WebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> serverFactoryCustomizer() {
+        return new WebServerFactoryCustomizer<ConfigurableServletWebServerFactory>() {
+            @Override
+            public void customize(ConfigurableServletWebServerFactory factory) {
+                factory.setPort(8083);
+            }
+        };
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
